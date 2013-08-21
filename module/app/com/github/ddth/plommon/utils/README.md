5d46f6916725f1b531dd917fe11f947a992b9ae4
btnguyen2k <btnguyen2k@gmail.com>
2013-08-21 16:41:58 +0700

    Update doc

--- module/app/com/github/ddth/plommon/utils/README.md
+++ module/app/com/github/ddth/plommon/utils/README.md
@@ -5,6 +5,8 @@ Plommon - com.github.ddth.plommon.utils
 -----------
 Many times you just want to serialize your Java object to JSON-string and vice-versa, without touching any `JsonNode` object. This utility class provides methods to do just that.
 
+Sample usage:
+
 ```java
 import com.github.ddth.plommon.utils.JsonUtils;
 . . .
@@ -16,4 +18,38 @@ String jsonString = JsonUtils.toJsonString(objList);
 . . .
 
 List<?> anotherObj = JsonUtils.fromJsonString(jsonString, List.class);
+. . .
+```
+
+`SessionUtils`
+--------------
+Session utilities with enhanced functionality:
+
+* Complex data structure for session items (Session items are automatically serialized/deserialized using JSON format). 
+* TTL (time-to-live): session items can have expiry.
+
+Sample usage:
+
+```java
+import com.github.ddth.plommon.utils.SessionUtils;
+. . .
+
+/*
+ * <code>true</code> (default): session item's expiry is automatically refreshed everytime it is accessed.
+ */
+SessionUtils.autoTouch = true;
+. . .
+
+
+Map<String, Object> user = new HashMap<String, Object>();
+user.put("first_name", "Thanh");
+user.put("last_name", "Nguyen"); 
+. . .
+
+//time-to-live = 300 seconds
+SessionUtils.setSession("user", user, 300);
+. . .
+
+Object obj = SessionUtils.getSession("user");
+. . .
 ```
--- module/app/com/github/ddth/plommon/utils/SessionUtils.java
+++ module/app/com/github/ddth/plommon/utils/SessionUtils.java
@@ -11,13 +11,36 @@ import play.mvc.Controller;
  * <ul>
  * <li>Complex data structure for session items. Session items are automatically
  * serialized/deserialized using JSON format.</li>
- * <li>TTL: session items can have expiry.</li>
+ * <li>TTL (time-to-live): session items can have expiry.</li>
  * </ul>
  * 
  * @author Thanh Nguyen <btnguyen2k@gmail.com>
  * @since 0.1.0
  */
 public class SessionUtils {
+
+	/**
+	 * <code>true</code> (default): control whether session item's expiry is automatically
+	 * refreshed everytime it is accessed.
+	 */
+	public static boolean autoTouch = true;
+
+	private static final String KEY_EXPIRY = "e";
+	private static final String KEY_VALUE = "v";
+
+	/**
+	 * Gets a session item.
+	 * 
+	 * Session item's expiry will be automatically refreshed if
+	 * {@link #autoTouch} is <code>true</code>.
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public static Object getSession(String key) {
+		return getSession(key, autoTouch);
+	}
+
 	/**
 	 * Gets a session item.
 	 * 
@@ -26,7 +49,7 @@ public class SessionUtils {
 	 *         or it has been expired
 	 */
 	@SuppressWarnings("unchecked")
-	public static Object getSession(String key) {
+	public static Object getSession(String key, boolean touch) {
 		String sValue = Controller.session(key);
 		if (sValue == null) {
 			return null;
@@ -34,16 +57,20 @@ public class SessionUtils {
 		try {
 			Map<String, Object> sEntry = JsonUtils.fromJsonString(sValue,
 					Map.class);
-			Long expiry = DPathUtils.getValue(sEntry, "expiry", Long.class);
+			Long expiry = DPathUtils.getValue(sEntry, KEY_EXPIRY, Long.class);
 			if (expiry == null
 					|| expiry.longValue() > System.currentTimeMillis()) {
-				return sEntry.get("value");
+				Object result = sEntry.get(KEY_VALUE);
+				if (touch && expiry != null) {
+					setSession(key, result, expiry / 1000);
+				}
+				return result;
 			} else {
 				Controller.session().remove(key);
 				return null;
 			}
 		} catch (Exception e) {
-			return null;
+			return sValue;
 		}
 	}
 
@@ -67,9 +94,9 @@ public class SessionUtils {
 	 */
 	public static void setSession(String key, Object value, long ttl) {
 		Map<String, Object> sEntry = new HashMap<String, Object>();
-		sEntry.put("value", value);
+		sEntry.put(KEY_VALUE, value);
 		if (ttl > 0) {
-			sEntry.put("expiry", System.currentTimeMillis() + ttl * 1000);
+			sEntry.put(KEY_EXPIRY, System.currentTimeMillis() + ttl * 1000);
 		}
 		Controller.session(key, JsonUtils.toJsonString(value));
 	}
