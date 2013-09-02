71cf9ff9fbbb6c008a619b7c466795ad2f1bf601
btnguyen2k <btnguyen2k@gmail.com>
2013-09-03 00:46:01 +0700

    Release v0.2.1-SNAPSHOT

--- RELEASE-NOTES.md
+++ RELEASE-NOTES.md
@@ -1,6 +1,10 @@
 play-module-plommon
 ===================
 
+0.2.1-SNAPSHOT - 2013-09-01
+---------------------------
+- Snapshot release: new method `JsonUtils.fromJsonString(String)` and minor enhancement in method `JsonUtils.fromJsonString(String, Class)`.
+
 
 0.2.0-SNAPSHOT - 2013-08-28
 ---------------------------
@@ -10,6 +14,7 @@ play-module-plommon
 0.1.0 - 2013-08-26
 ------------------
 - Stable release: version 0.1.0.
+
 
 0.1.0-SNAPSHOT - 2013-08-20
 ---------------------------
--- module/app/com/github/ddth/plommon/utils/JsonUtils.java
+++ module/app/com/github/ddth/plommon/utils/JsonUtils.java
@@ -17,25 +17,46 @@ import play.libs.Json;
  * @since 0.1.0
  */
 public class JsonUtils {
-	/**
-	 * Serializes an object to Json string.
-	 * 
-	 * @param obj
-	 * @return
-	 */
-	public static String toJsonString(Object obj) {
-		return Json.stringify(Json.toJson(obj));
-	}
+    /**
+     * Serializes an object to Json string.
+     * 
+     * @param obj
+     * @return
+     */
+    public static String toJsonString(Object obj) {
+        return Json.stringify(Json.toJson(obj));
+    }
 
-	/**
-	 * Deserializes a Json string.
-	 * 
-	 * @param jsonString
-	 * @param clazz
-	 * @return
-	 */
-	public static <T> T fromJsonString(String jsonString, Class<T> clazz) {
-		JsonNode jsonNode = Json.parse(jsonString);
-		return jsonNode != null ? Json.fromJson(jsonNode, clazz) : null;
-	}
+    /**
+     * Deserializes a Json string.
+     * 
+     * @param jsonString
+     * @param clazz
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> T fromJsonString(String jsonString, Class<T> clazz) {
+        Object obj = fromJsonString(jsonString);
+        if (obj == null || clazz == null) {
+            return null;
+        }
+        if (clazz.isAssignableFrom(obj.getClass())) {
+            return (T) obj;
+        } else {
+            String msg = "Can not cast to class [" + clazz + "]!";
+            throw new ClassCastException(msg);
+        }
+    }
+
+    /**
+     * Deserializes a Json string.
+     * 
+     * @param jsonString
+     * @return
+     * @since 0.2.1
+     */
+    public static Object fromJsonString(String jsonString) {
+        JsonNode jsonNode = Json.parse(jsonString);
+        return jsonNode != null ? Json.fromJson(jsonNode, Object.class) : null;
+    }
 }
