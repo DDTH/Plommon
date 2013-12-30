15a18da3a37c4b6fc5581b42388542b68006834e
Thanh Nguyen <btnguyen2k@gmail.com>
2013-12-31 02:13:22 +0700

    v0.4.4: bug fixes

--- module/project/Build.scala
+++ module/project/Build.scala
@@ -5,7 +5,7 @@ import play.Project._
 object ApplicationBuild extends Build {
 
   val appName         = "play-module-plommon"
-  val appVersion      = "0.4.4-SNAPSHOT"
+  val appVersion      = "0.4.4"
 
   val appDependencies = Seq(
     // Add your project dependencies here,
