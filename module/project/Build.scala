d437fe52d50eb41c64f9cff0ad6c93225acfea62
Thanh Nguyen <btnguyen2k@gmail.com>
2013-12-31 14:21:40 +0700

    Release v0.4.5

--- module/project/Build.scala
+++ module/project/Build.scala
@@ -5,7 +5,7 @@ import play.Project._
 object ApplicationBuild extends Build {
 
   val appName         = "play-module-plommon"
-  val appVersion      = "0.4.5-SNAPSHOT"
+  val appVersion      = "0.4.5"
 
   val appDependencies = Seq(
     // Add your project dependencies here,
